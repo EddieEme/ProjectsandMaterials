28dfad8f754ec832f8d4e8e92c9f3c683e1eb440
Emeremnu Edison <edisonemeremnu@gmail.com>
2025-02-25 00:01:46 +0100

    fixed x-frame settings

--- projectsandmaterials/settings.py
+++ projectsandmaterials/settings.py
@@ -239,6 +239,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
